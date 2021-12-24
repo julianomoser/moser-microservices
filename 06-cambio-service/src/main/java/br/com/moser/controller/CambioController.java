e9a868656337269ef0273967fcf35a167e13bedd
julianomoser <73764784+julianomoser@users.noreply.github.com>
2021-12-23 23:41:42 -0300

    Add Distributed Tracing com Docker,Zipkin,Eureka e Sleuthr

--- 06-cambio-service/src/main/java/br/com/moser/controller/CambioController.java
+++ 06-cambio-service/src/main/java/br/com/moser/controller/CambioController.java
@@ -4,6 +4,8 @@ import br.com.moser.model.Cambio;
 import br.com.moser.repository.CambioRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +24,7 @@ import java.math.RoundingMode;
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    private Logger logger = LoggerFactory.getLogger(CambioController.class);
     @Autowired
     private Environment environment;
     @Autowired
@@ -32,6 +35,8 @@ public class CambioController {
     public Cambio getCambio(@PathVariable("amount")BigDecimal amount,
                             @PathVariable("from")String from,
                             @PathVariable("to")String to) {
+
+        logger.info("getCambio is called whit -> {}, {} and {}", amount, from, to);
         var cambio = cambioRepository.findByFromAndTo(from, to);
         if(cambio == null) throw new RuntimeException("Currency Unsupported");
         var port = environment.getProperty("local.server.port");
